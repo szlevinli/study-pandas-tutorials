1cd2fc987059d2a42139c1ffb6772c0d6eb2b913
levin <3259963@qq.com>
2022-02-10 18:01:57 +0800

    update new_a.py

--- Work/src/new_a.py
+++ Work/src/new_a.py
@@ -2,6 +2,9 @@ import argparse
 from pathlib import Path
 import pandas as pd
 
+# lp: last period
+# tp: this period
+
 arg_parser = argparse.ArgumentParser(
     description='根据本期数据和上期数据统计不规范合同操作情况.'
 )
@@ -30,4 +33,36 @@ arg_parser.add_argument(
     help='文件目录. 基于当前目录.'
 )
 
-args = arg_parser.parse_args()
+arg_parser.add_argument(
+    '--tp-sheet',
+    action='store',
+    help='本期工作表名称. 默认第一个工作表'
+)
+
+arg_parser.add_argument(
+    '--lp-sheet',
+    action='store',
+    help='上期工作表名称. 默认第一个工作表'
+)
+
+arg_parser.add_argument(
+    '--keys',
+    action='store',
+    nargs='+',
+    type=str,
+    default='合同编号',
+    help='关键字. 多个用空格分隔. 默认: 合同编号'
+)
+
+
+def get_data_path(p: str) -> Path:
+    return Path.cwd() / p
+
+
+def get_lp_path(p: str) -> Path:
+    return Path.cwd() / p
+
+
+if __name__ == '__main__':
+    args = arg_parser.parse_args()
+    print(vars(args))
